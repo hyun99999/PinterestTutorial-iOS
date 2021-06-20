80600e2982008023f6626a6803db670935e71bdc
hyungyu Kim <ypsophila97@icloud.com>
2021-06-20 21:56:19 +0900

    [chore] 프로젝트 생성 및 .gitignore 추가
    ### Descriptions
    - 프로젝트 생성
    - .gitignore 추가

---
+++ PinterestTutorial-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinterestTutorial-iOS
+//
+//  Created by kimhyungyu on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
